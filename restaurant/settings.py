1d458022afc7de7982c8c0e96ef89151227f25b1
Oscar Butler <oscarbutler48@gmail.com>
2024-04-01 10:35:50 +0000

    Add allowed origins to settings.py

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u_yff%tq+!9f^1dzfs4mc802_sc@#$-ry!^wu4i*k2q^ogsx9b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-oscarbutler-restaurant-m3d50u4ixqt.ws-eu110.gitpod.io']
 
 
 # Application definition
