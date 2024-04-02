05c8ff1898ebeee8b1e18aa5c4cd1d5038f0b199
Oscar Butler <oscarbutler48@gmail.com>
2024-04-02 13:45:58 +0000

    Add fields on to booking system

--- booking/models.py
+++ booking/models.py
@@ -4,9 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class BookingSystem(models.Model):
     User = models.ForeignKey(User, on_delete=models.CASCADE)
-    Name = 
-    Phone_number =
-    Email =
-    Data =
-    Time =
-    Number_of_people =
+    Name = models.CharField(max_length=50)
+    Phone_number = models.CharField(max_length=20)
+    Email = models.EmailField()
+    Date = models.DateField()
+    Time = models.TimeField()
