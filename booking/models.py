a53d3a75974574420049a3173f40c80493b73c6a
Oscar Butler <oscarbutler48@gmail.com>
2024-04-02 12:08:34 +0000

    Add other keys to booking system

--- booking/models.py
+++ booking/models.py
@@ -3,3 +3,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class BookingSystem(models.Model):
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
+    Name = 
+    Phone_number =
+    Email =
+    Data =
+    Time =
+    Number_of_people =
