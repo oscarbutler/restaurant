a67244b9d2b2b47c7f0fc7f39a9e4f1028f873a7
Oscar Butler <oscarbutler48@gmail.com>
2024-04-07 13:09:39 +0000

    Add links for admin page

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('booking/', message, name='booking'),
+    path("", include("booking.urls"), name="booking-urls"),
+
 ]
