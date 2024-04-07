244179981b49e2a8e89ea406163007e0e1c07cda
Oscar Butler <oscarbutler48@gmail.com>
2024-04-07 13:03:51 +0000

    Make index template show as home page

--- booking/views.py
+++ booking/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import admin
 
 # Create your views here.
 def message(request):
     return HttpResponse("Test")
 
-def booking(request):
+def home(request):
+    return render(request, 'index.html')
