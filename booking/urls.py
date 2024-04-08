d9ae6c86897d0b4f634d584370dd2be09968e406
Oscar Butler <oscarbutler48@gmail.com>
2024-04-08 15:12:14 +0000

    Fix url paths

--- booking/urls.py
+++ booking/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('menu', views.menu, name='menu'),
-    path('info', views.info, name='info'),
-    path('drinks', views.menu_drinks, name='menu-drinks'),
-    path('starters', views.menu_starters, name='menu-starters'),
-    path('mains', views.menu_main, name='menu-main'),
-    path('desserts', views.menu_desserts, name='menu-desserts'),
+    path('menu.html', views.menu, name='menu'),
+    path('info.html', views.info, name='info'),
+    path('menu-drinks.html', views.menu_drinks, name='menu-drinks'),
+    path('menu-starters.html', views.menu_starters, name='menu-starters'),
+    path('menu-main.html', views.menu_main, name='menu-main'),
+    path('menu-desserts.html', views.menu_desserts, name='menu-desserts'),
+
 ]
