39fa9653498982477c57ca270e9f571fa1f61560
Oscar Butler <oscarbutler48@gmail.com>
2024-04-08 14:59:31 +0000

    Add links to view templates

--- booking/urls.py
+++ booking/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('menu', views.menu, name='menu'),
     path('info', views.info, name='info'),
+    path('drinks', views.menu_drinks, name='menu-drinks'),
+    path('starters', views.menu_starters, name='menu-starters'),
+    path('mains', views.menu_main, name='menu-main'),
+    path('desserts', views.menu_desserts, name='menu-desserts'),
 ]
