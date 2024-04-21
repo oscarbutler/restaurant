17342ecf4872d679aab0269bfdc0a9a6d0bf992b
Oscar Butler <oscarbutler48@gmail.com>
2024-04-21 12:28:13 +0000

    Add widget for the date field

--- booking/forms.py
+++ booking/forms.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 from .models import BookingSystem
 from django import forms
 
-class reservationForm():
+class reservationForm(forms.ModelForm):
     class Meta:
         model = BookingSystem
         fields = ['Name', 'Phone_number', 'Email', 'Date', 'Time', 'Number_Of_People', ]
+        widgets = {
+            'Date': forms.DateInput(attrs={'type': 'date'}),
+        }
