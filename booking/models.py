db9647d9866dd7ff1b9c0896dc52e828318576a9
Oscar Butler <oscarbutler48@gmail.com>
2024-04-25 12:07:46 +0000

    Remove capitilization of words

--- booking/models.py
+++ booking/models.py
@@ -1,15 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.views import generic
-# from .models import BookingSystem
 
-# Create your models here.
 class BookingSystem(models.Model):
-    User = models.ForeignKey(User, on_delete=models.CASCADE)
-    Name = models.CharField(max_length=50)
-    Phone_number = models.CharField(max_length=20)
-    Email = models.EmailField()
-    Date = models.DateField()
-    Time = models.TimeField()
-    Number_Of_People = models.IntegerField()
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    date = models.DateField()
+    time = models.TimeField()
+    number_of_people = models.IntegerField()
