7a1455940af62e7adb0bce57d82454a31bebd79f
Oscar Butler <oscarbutler48@gmail.com>
2024-05-16 13:16:39 +0000

    Add register urls

--- booking/urls.py
+++ booking/urls.py
@@ -14,5 +14,13 @@ urlpatterns = [
     path('booking.html', views.booking, name='booking'),
     path('make_booking.html', views.MakeBooking, name='make-booking'),
     path('register/', views.register, name="register"),
+    path('register/menu-drinks.html', views.menu_drinks, name='menu-drinks'),
+    path('register/menu-main.html', views.menu_main, name='menu-main'),
+    path('register/menu-starters.html', views.menu_starters, name='menu-starters'),
+    path('register/menu-desserts.html', views.menu_desserts, name='menu-drinks'),
+    path('register/index.html', views.home, name='home'),
+    path('register/booking.html', views.booking, name='booking'),
+    path('register/make_booking.html', views.MakeBooking, name='make-booking'),
+    path('register/menu-drinks.html', views.menu_drinks, name='menu'),
     path('register/menu-drinks.html', views.menu_drinks, name='menu'),
 ]
