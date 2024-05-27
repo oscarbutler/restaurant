a96387a4c54957feea5f962ffb23498b75ba24a0
Oscar Butler <oscarbutler48@gmail.com>
2024-05-27 18:58:43 +0000

    Add what is shown in booking admin

--- booking/admin.py
+++ booking/admin.py
@@ -4,3 +4,4 @@ from .models import BookingSystem
 # Register your models here.
 @admin.register(BookingSystem)
 class BookingAdmin(admin.ModelAdmin):
+    list_display = ['name','date', 'time', 'number_of_people']
