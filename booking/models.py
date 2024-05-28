e678f91255f0788c4aebef8999159896fd5b8608
Oscar Butler <oscarbutler48@gmail.com>
2024-05-28 14:52:22 +0000

    Link the choices charfield to constants.py lists

--- booking/models.py
+++ booking/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.views import generic
+from datetime import time
+from .constants import different_times, amount_of_people
+import datetime
 
 class BookingSystem(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=20)
     email = models.EmailField()
     date = models.DateField()
-    time = models.TimeField()
-    number_of_people = models.IntegerField()
+    time = models.CharField(choices=different_times)
+    number_of_people = models.CharField(choices=amount_of_people)
+    
+
