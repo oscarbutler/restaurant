d82de9533683a061bf6097ac579c68f111580135
Oscar Butler <oscarbutler48@gmail.com>
2024-06-01 21:40:39 +0000

    Add static root and static files

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -145,3 +145,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+staticfiles_dirs = [
+    BASE_DIR / "static",
+]
