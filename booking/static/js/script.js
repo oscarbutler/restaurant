7388c3ec8997407389781d84dbf876216b7ff3e8
Oscar Butler <oscarbutler48@gmail.com>
2024-06-02 14:14:59 +0000

    Add slideshow index variable

--- booking/static/js/script.js
+++ booking/static/js/script.js
@@ -1,14 +1,5 @@
 let availableTimes = [
     {times: ['13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00']}
-]
+];
 
-function Times(){
-    const chooseTime = document.getElementById("id_time")
-}
-function Slideshow(){
-    const SlideshowImages = [
-        "{% static 'images/dining-image.jpg' %}",
-        "{% static 'images/steak-image.jpg' %}",
-    ]
-
-}
+let SlideshowIndex = 0;
