f420d51cc63a59fdad7e2871930343c15ad22f09
Oscar Butler <oscarbutler48@gmail.com>
2024-06-02 18:37:51 +0000

    Add path for no additional links

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("booking.urls"), name="booking-urls"),
-
+    path('', include('django.contrib.auth.urls')),
 ]
