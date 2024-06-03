cd3f51d93375401e0fcb8cc1d49db7588328c156
Oscar Butler <oscarbutler48@gmail.com>
2024-06-03 18:07:12 +0000

    Add allauth and accounts

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("booking.urls"), name="booking-urls"),
     path('', include('django.contrib.auth.urls')),
+    path('accounts/', include("allauth.urls"))
 ]
