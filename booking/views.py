ac1f82fcc0558e49eba04ff103fddea84e0130d7
Oscar Butler <oscarbutler48@gmail.com>
2024-06-03 18:02:06 +0000

    Change all function return

--- booking/views.py
+++ booking/views.py
@@ -13,28 +13,28 @@ def message(request):
     return HttpResponse("Test")
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'allauth/account/index.html')
 
 def menu(request):
-    return render(request, 'menu.html')
+    return render(request, 'allauth/account/menu.html')
 
 def info(request):
-    return render(request, 'info.html')
+    return render(request, 'allauth/account/info.html')
 
-def menu_main(request):
-    return render(request, 'menu-main.html')
+# def menu_main(request):
+#     return render(request, 'menu-main.html')
 
 def menu_starters(request):
-    return render(request, 'menu-starters.html')
+    return render(request, 'allauth/account/menu-starters.html')
 
-def menu_desserts(request):
-    return render(request, 'menu-desserts.html')
+# def menu_desserts(request):
+#     return render(request, 'menu-desserts.html')
 
 def booking(request):
-    return render(request, 'booking.html')
+    return render(request, 'allauth/account/booking.html')
 
 def menu_drinks(request):
-    return render(request, 'menu-drinks.html')
+    return render(request, 'allauth/account/menu-drinks.html')
 
 @login_required
 def MakeBooking(request):
@@ -44,10 +44,10 @@ def MakeBooking(request):
             booking = form.save(commit=False)
             booking.user_id = request.user.id 
             booking.save()
-            return render(request, 'booking_success.html')
+            return render(request, 'allauth/account/index.html')
     else:
         form = reservationForm()
-    return render(request, 'make_booking.html', {'form': form})
+    return render(request, 'allauth/account/make_booking.html', {'form': form})
 
 def register(response):
     if response.method == "POST":
@@ -55,14 +55,14 @@ def register(response):
         if form.is_valid():
             form.save()
         
-        return redirect("index.html")
+        return redirect("allauth/account/index.html")
     else:
         form = RegisterForm()
 
-        return render(response, "register.html", {"form":form})
+        return render(response, "allauth/account/register.html", {"form":form})
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'allauth/account/login.html')
 
 @login_required
 def view_bookings(request):
@@ -72,6 +72,12 @@ def view_bookings(request):
         booking_id = request.POST.get('booking_id')
         booking = get_object_or_404(BookingSystem, pk=booking_id)
         booking.delete()
-        return redirect('view_bookings.html')
+        return redirect('allauth/account/view_bookings.html')
 
-    return render(request, 'view_bookings.html', {'bookings': bookings})
+    return render(request, 'allauth/account/view_bookings.html', {'bookings': bookings})
+
+def menu_desserts(request):
+    return render(request, 'allauth/account/menu-desserts.html')
+
+def menu_main(request):
+    return render(request, 'allauth/account/menu-main.html')
