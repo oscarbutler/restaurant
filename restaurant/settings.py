8b67e0d1b41b4b6565e39642bd32f0c23714b2cd
Oscar Butler <oscarbutler48@gmail.com>
2024-09-20 18:50:01 +0000

    Add message tags

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -161,3 +161,13 @@ STATICFILES_DIRS = [
 
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
