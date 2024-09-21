ccd02329d9e667e2ca6b7392bb6c91b4a163ae91
Oscar Butler <oscarbutler48@gmail.com>
2024-09-21 00:18:25 +0000

    Validate the code

--- booking/views.py
+++ booking/views.py
@@ -9,28 +9,36 @@ from .forms import RegisterForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
+
 def message(request):
     return HttpResponse("Test")
+
 
 def home(request):
     return render(request, 'allauth/account/index.html')
 
+
 def menu(request):
     return render(request, 'allauth/account/menu.html')
+
 
 def info(request):
     return render(request, 'allauth/account/info.html')
 
     return render(request, 'menu-main.html')
 
+
 def menu_starters(request):
     return render(request, 'allauth/account/menu-starters.html')
+
 
 def booking(request):
     return render(request, 'allauth/account/booking.html')
 
+
 def menu_drinks(request):
     return render(request, 'allauth/account/menu-drinks.html')
+
 
 @login_required
 def MakeBooking(request):
@@ -38,7 +46,7 @@ def MakeBooking(request):
         form = reservationForm(request.POST)
         if form.is_valid():
             booking = form.save(commit=False)
-            booking.user_id = request.user.id 
+            booking.user_id = request.user.id
             booking.save()
             messages.success(request, 'Booking successfully made!')
             return render(request, 'allauth/account/index.html')
@@ -46,20 +54,24 @@ def MakeBooking(request):
         form = reservationForm()
     return render(request, 'allauth/account/make_booking.html', {'form': form})
 
+
 def register(response):
     if response.method == "POST":
         form = RegisterForm(response.POST)
         if form.is_valid():
             form.save()
-        
+
         return redirect("index.html")
     else:
         form = RegisterForm()
 
-        return render(response, "allauth/account/register.html", {"form":form})
+        return render(response, "allauth/account/register.html",
+                                {"form": form})
+
 
 def login(request):
     return render(request, 'allauth/account/login.html')
+
 
 @login_required
 def view_bookings(request):
@@ -71,13 +83,17 @@ def view_bookings(request):
         booking.delete()
         return redirect('view_bookings.html')
 
-    return render(request, 'allauth/account/view_bookings.html', {'bookings': bookings})
+    return render
+    (request, 'allauth/account/view_bookings.html', {'bookings': bookings})
+
 
 def menu_desserts(request):
     return render(request, 'allauth/account/menu-desserts.html')
 
+
 def menu_main(request):
     return render(request, 'allauth/account/menu-main.html')
+
 
 def edit_booking(request, booking_id):
     booking = get_object_or_404(BookingSystem, id=booking_id)
@@ -85,9 +101,11 @@ def edit_booking(request, booking_id):
         form = reservationForm(request.POST, instance=booking)
         if form.is_valid():
             form.save()
-            messages.success(request, 'You Have Successfully Changed Your Reservation!')
+            messages.success(
+                request, 'You Have Successfully Changed Your Reservation!')
             return redirect('view_bookings')
     else:
         form = reservationForm(instance=booking)
-    
-    return render(request, 'allauth/account/edit-booking.html', {'form': form, 'booking': booking})
+
+    return render(request, 'allauth/account/edit-booking.html',
+                  {'form': form, 'booking': booking})
