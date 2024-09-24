900e9feac6f07ad2789ae50a5923de9eee9c78e5
Oscar Butler <oscarbutler48@gmail.com>
2024-09-24 02:24:45 +0000

    Create reviews admin

--- booking/admin.py
+++ booking/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import BookingSystem
+from .models import BookingSystem, Reviews
 
 @admin.register(BookingSystem)
 class BookingAdmin(admin.ModelAdmin):
     list_display = ['name','date', 'time', 'number_of_people']
     list_filter = ['date', 'time']
     search_fields = ['name', 'phone_number', 'email', 'date']
+
+@admin.register(Reviews)
+class ReviewsAdmin(admin.ModelAdmin):
