b3e50b49c7bc0309c95cb2323bad7341e302fbe5
Oscar Butler <oscarbutler48@gmail.com>
2024-09-24 10:33:42 +0000

    Final changes

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,11 +1,11 @@
-let availableTimes = [
+availableTimes = [
     {times: ['13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00']}
 ];
 
-let SlideshowIndex = 0;
+SlideshowIndex = 0;
 
 function changeSlideshowImage() {
-    const img = document.getElementById('slideshowImage');
+    img = document.getElementById('slideshowImage');
     SlideshowIndex = (SlideshowIndex + 1) % SlideshowImagesList.length;
     img.src = SlideshowImagesList[SlideshowIndex];
 }
